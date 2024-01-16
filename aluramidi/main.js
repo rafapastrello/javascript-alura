78d0606fe18a983194ccf26946dfdc1f00e59a34
rafllosilva <125152977+rafllosilva@users.noreply.github.com>
2024-01-16 10:59:05 -0400

    Adiciona Pastas
    aluramidi, logica e midi-alura

---
+++ aluramidi/main.js
@@ -0,0 +1,27 @@
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//enquanto
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    //template string
+    const idAudio = `#som_${instrumento}`;
+    
+    console.log(idAudio);
+    
+    tecla.onclik = function () {
+        tocaSom('#som_tecla_pom');
+    }
+    
+    contador = contador + 1;
+
+    console.log(contador);
+
+}
